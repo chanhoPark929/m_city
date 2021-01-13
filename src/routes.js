e970361aab9a9e129fd33d8ef15d355b0f30c018
Chanho Park <chanho929@outlook.com>
2021-01-14 01:31:48 +0800

    update landing page animation

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import Layout from './Hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Components/home';
 
 const Routes = (props) => {
   return (
     <Layout>
-      second line
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
     </Layout>
   );
 }
