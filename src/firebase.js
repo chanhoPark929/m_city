7457df09e8c8971f4851327dd512fe21c096e2d9
Chanho Park <chanho929@outlook.com>
2021-01-14 03:41:47 +0800

    configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+    apiKey: "AIzaSyA7PkM10vGv5cLtM1liumtIHLlJ0AttOC8",
+    authDomain: "manchester-city-3082f.firebaseapp.com",
+    databaseURL: "https://manchester-city-3082f-default-rtdb.firebaseio.com",
+    projectId: "manchester-city-3082f",
+    storageBucket: "manchester-city-3082f.appspot.com",
+    messagingSenderId: "446874574319",
+    appId: "1:446874574319:web:d73bf7778ce8002769aa1a",
+    measurementId: "G-SBDC5YLKD3"   
+}
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+
+firebaseDB.ref('matches').once('value').then((snapshot)=> {
+    console.log(snapshot.val);
+})
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './Resources/css/app.css';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import './firebase';
 
 const App = () => {
   return (
