b5956e02296348939b8075ea7d58fc03b35679a4
Chanho Park <chanho929@outlook.com>
2021-01-15 00:52:07 +0800

    enrolling, starting with form fields

---
+++ src/Components/ui/formFields.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const FormField = ({formdata, id}) => {
+
+    const renderTemplate = () => {
+        let formTemplate = null;
+
+        switch (formdata.element) {
+            case 'input':
+                formTemplate = (
+                    <div>
+                        <input 
+                            {...formdata.config}
+                            value={formdata.value}
+                        />
+                    </div>
+                )
+                break;
+        
+            default:
+                formTemplate = null;
+                break;
+        }
+        return formTemplate;
+    }
+
+    return (
+        <div>
+            {renderTemplate()}
+        </div>
+    )
+}
+
+export default FormField;
