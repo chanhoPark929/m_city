015f21eefa3df79ae739788b77eddd37aeaccc23
Chanho Park <chanho929@outlook.com>
2021-01-19 00:32:04 +0800

    matches endpoint admin dashboard

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import Layout from './Hoc/Layout';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
 import PrivateRoute from './Components/authRoutes/privateRoutes';
 import PublicRoute from './Components/authRoutes/publicRoutes';
+import AdminMatches from './Components/admin/matches';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+      <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches}/>
         <PrivateRoute {...props} path='/dashboard' exact component={Dashboard}/>
         <PublicRoute {...props} restricted={true} path="/" exact component={Home} />
         <PublicRoute {...props} restricted={false} path='/sign_in' exact component={SignIn} />
